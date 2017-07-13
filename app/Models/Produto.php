33693c5e35c6832235f182cf03639448230cb9f7
Igor Abreu <igorabreu.ds@gmail.com>
2017-07-13 00:06:40 -0300

    Adicionando modelo de produto

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Produto extends Model
 {
-    //
+    protected $table = "produtos";
+
+    protected $fillable = [
+        'nome',
+        'descricao',
+        'preco',
+        'pontos',
+        'preco_pontos'
+    ];
+
+    public function vendas()
+    {
+        return $this->hasMany(Venda::class);
+    }
 }
