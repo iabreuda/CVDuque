6252459f2f82b0a35d1a7c046fb2527dba9f9cf5
Igor Abreu <igorabreu.ds@gmail.com>
2017-07-13 00:06:51 -0300

    Adicionando modelo de venda

--- app/Models/Venda.php
+++ app/Models/Venda.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Venda extends Model
 {
-    //
+    protected $table = "vendas";
+
+    protected $fillable = [
+        'pontos',
+        'dt_venda',
+        'comentario',
+        'quantidade',
+        'cliente_id',
+        'produto_id'
+    ];
+    public function cliente()
+    {
+        return $this->belongsTo(Cliente::class);
+    }
+
+    public function produto()
+    {
+        return $this->belongsTo(Produto::class);
+    }
 }
