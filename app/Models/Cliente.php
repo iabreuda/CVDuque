677b3508034e137c945286a37f3405f395a28e69
Igor Abreu <igorabreu.ds@gmail.com>
2017-07-13 00:06:26 -0300

    Adicionando modelo de cliente

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cliente extends Model
 {
-    //
+    protected $table = "clientes";
+
+    protected $fillable = [
+        'nome',
+        'cpf',
+        'telefone',
+        'dt_nascimento',
+        'email',
+        'celular',
+        'cep',
+        'logradouro',
+        'numero',
+        'complemento',
+        'bairro',
+        'estado',
+        'municipio'
+    ];
+
+    public function compras()
+    {
+        return $this->hasMany(Venda::class);
+    }
 }
