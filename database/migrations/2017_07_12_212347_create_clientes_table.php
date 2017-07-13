17f054170a548927da116a6fd8af6244bacffbfb
Igor Abreu <igorabreu.ds@gmail.com>
2017-07-13 00:05:26 -0300

    Definindo atributos nullable

--- database/migrations/2017_07_12_212347_create_clientes_table.php
+++ database/migrations/2017_07_12_212347_create_clientes_table.php
@@ -18,13 +18,13 @@ class CreateClientesTable extends Migration
             $table->date("dt_nascimento");
             $table->string("nome", 50);
             $table->string("email", 100)->unique();
-            $table->string("telefone", 10)->nulable();
-            $table->string("celular", 11)->nulable();
+            $table->string("telefone", 10)->nullable();
+            $table->string("celular", 11)->nullable();
             $table->string("cpf", 11)->unique();
             $table->string("cep", 8);
             $table->string("logradouro", 250);
             $table->string("numero", 5);
-            $table->string("complemento", 50)->nulable();
+            $table->string("complemento", 50)->nullable();
             $table->string("bairro", 100);
             $table->string("estado", 100);
             $table->string("municipio", 100);
